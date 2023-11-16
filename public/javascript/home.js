3ff61da16e892132255b68d51adab94e405a57f8
Pedro Tissone <pedrotissone@gmail.com>
2023-11-16 10:03:59 -0300

    primer commit

---
+++ public/javascript/home.js
@@ -0,0 +1,7 @@
+
+//                                            AJUSTAR ALTURA DINAMICAMENTE DEL TEXTAREA
+
+function ajustarAltura(ele) { // El (ele) es el <textarea>
+    // ele.style.height = "auto"; //No necesito esta linea porque ya le di auto en css
+    ele.style.height = (ele.scrollHeight) + "px";
+}
